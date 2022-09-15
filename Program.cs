e5d8de9b9f538ac916a84eec7530aa71414fa0af
Pekka Alaluukas <Pekka.Alaluukas@oamk.fi>
2022-09-15 10:51:47 +0300

    Student class works

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Exercise8;
+
 Console.WriteLine("Hello, World!");
+
+Student objectStudent=new Student("Teppo","Testi");
+Console.WriteLine(objectStudent.Fname+" "+objectStudent.Lname);
